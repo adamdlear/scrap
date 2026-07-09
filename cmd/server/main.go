1114f15168de7ed908a02e4afeef15163a222c06
Adam Lear <adamlear12@gmail.com>
2026-07-08 22:08:05 -0500

    feat: add basic websocket handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"slices"
+	"strings"
+
+	"golang.org/x/net/websocket"
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +15,26 @@ func health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK")
 }
 
+func wsHandler(ws *websocket.Conn) {
+	for {
+		var msg string
+		if err := websocket.Message.Receive(ws, &msg); err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error: %v\n", err)
+		}
+		words := strings.Split(msg, " ")
+		slices.Reverse(words)
+		revMsg := strings.Join(words, " ")
+		websocket.Message.Send(ws, revMsg)
+	}
+}
+
 func main() {
 	http.HandleFunc("/health", health)
+	http.Handle("/ws", websocket.Handler(wsHandler))
+
+	fmt.Println("Listening on port :8080")
 	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adamdlear/docs
 
-go 1.24.3
+go 1.25.0
+
+require golang.org/x/net v0.57.0
